f089e92163d881925e8cdff0ba59fc44bc02e1fa
Horst Josef Grenz Meza <jgrenz@outlook.com>
2023-02-07 16:33:36 -0800

    implemented full functionality with story telling

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadStory()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle!
+        storyBrain.chooseStory(userChoice: userChoice)
+        loadStory()
+    }
+    
+    func loadStory(){
+        storyLabel.text = storyBrain.getCurrentStoryTitle()
+        
+        choice1Button.setTitle(storyBrain.getCurrentStoryChoice1Text(), for: .normal)
+        choice2Button.setTitle(storyBrain.getCurrentStoryChoice2Text(), for: .normal)
+    }
+    
 }
 
